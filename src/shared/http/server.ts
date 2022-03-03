9865874f83ed75ce54794cffa31460f24fbaa284
georginm <georginmatheus@gmail.com>
2022-03-03 12:26:15 -0300

    feat: add handle errors

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import routes from './routes';
+import handleError from './middleware/handleError';
 
 const app = express();
 
@@ -10,6 +11,8 @@ app.use(cors());
 app.use(express.json());
 
 app.use(routes);
+
+app.use(handleError);
 
 app.listen(PORT, () => {
   console.log(`Running server on port ${PORT}`);
