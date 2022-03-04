1d35222921db24db03bf62ac895bbb27c256c0ce
georginm <georginmatheus@gmail.com>
2022-03-04 17:42:33 -0300

    feat: create product entitiy

---
+++ src/modules/products/typeorm/entities/Product.ts
@@ -0,0 +1,38 @@
+import {
+  Entity,
+  PrimaryColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+import { v4 as uuidv4 } from 'uuid';
+
+@Entity({ database: 'products' })
+class Product {
+  @PrimaryColumn()
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column('decimal')
+  price: number;
+
+  @Column('int')
+  quantity: number;
+
+  @CreateDateColumn({ name: 'created_at' })
+  createdAt: Date;
+
+  @UpdateDateColumn({ name: 'updated_at' })
+  updatedAt: Date;
+
+  constructor() {
+    if (!this.id) {
+      this.id = uuidv4();
+    }
+  }
+}
+
+export { Product };
