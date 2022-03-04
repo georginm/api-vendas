1e9fa29850d886ae456565dcda5329173d26b736
georginm <georginmatheus@gmail.com>
2022-03-04 17:43:06 -0300

    feat: add ProductRepository

---
+++ src/modules/products/typeorm/repositories/ProductRepository.ts
@@ -0,0 +1,18 @@
+import { BadRequestError } from '@shared/errors/BadRequestError';
+import { EntityRepository, Repository } from 'typeorm';
+import { Product } from '../entities/Product';
+
+@EntityRepository(Product)
+class ProductRepository extends Repository<Product> {
+  public async findByName(name: string): Promise<Product> {
+    const product = await this.findOne({ where: { name } });
+
+    if (product) {
+      return product;
+    }
+
+    throw new BadRequestError('Product not exists');
+  }
+}
+
+export { ProductRepository };
