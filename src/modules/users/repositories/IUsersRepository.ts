e7f48633eb8984e4f63a933be1e89e0a16080867
georginm <georginmatheus@gmail.com>
2022-06-29 22:35:10 -0300

    chore: create users repository interface

---
+++ src/modules/users/repositories/IUsersRepository.ts
@@ -0,0 +1,11 @@
+import { ICreateUserDTO } from '../dto/ICreateUserDTO';
+import { User } from '../typeorm/entities/Users';
+
+interface IUsersRepository {
+  findByName(name: string): Promise<User | undefined>;
+  findByEmail(email: string): Promise<User | undefined>;
+  findById(id: string): Promise<User | undefined>;
+  createUser({ name, password, email }: ICreateUserDTO): Promise<User>;
+}
+
+export { IUsersRepository };
