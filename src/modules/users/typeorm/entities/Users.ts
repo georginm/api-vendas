6244242321a694e83969be64ad6b7aa57134e861
georginm <georginmatheus@gmail.com>
2022-06-29 21:40:21 -0300

    chore: add users entities

---
+++ src/modules/users/typeorm/entities/Users.ts
@@ -0,0 +1,38 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import { v4 as uuidV4 } from 'uuid';
+
+@Entity('users')
+class User {
+  @PrimaryColumn()
+  id: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  password: string;
+
+  @Column()
+  avatar: string;
+
+  @CreateDateColumn({ name: 'created_at' })
+  createdAt: Date;
+
+  @UpdateDateColumn({ name: 'updated_at' })
+  updatedAt: Date;
+
+  constructor() {
+    if (!this.id) this.id = uuidV4();
+  }
+}
+
+export { User };
