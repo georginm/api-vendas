f886bd17c3c62a35eab460362e9ae02d7186085b
georginm <georginmatheus@gmail.com>
2022-06-30 17:43:44 -0300

    chore: add generate method in the interface

--- src/modules/users/repositories/IUsertokensRepository.ts
+++ src/modules/users/repositories/IUsertokensRepository.ts
@@ -2,6 +2,7 @@ import { UserToken } from '../infra/typeorm/entities/UsersToken';
 
 interface IUserTokensRepository {
   findByToken(token: string): Promise<UserToken | undefined>;
+  generate(userId: string): Promise<UserToken | undefined>;
 }
 
 export { IUserTokensRepository };
