863c0b031742f2dc644cabcf9852d54e96659db3
georginm <georginmatheus@gmail.com>
2022-06-29 23:03:47 -0300

    chore: import user routers

--- src/shared/infra/express/routes/index.ts
+++ src/shared/infra/express/routes/index.ts
@@ -1,8 +1,10 @@
 import { productRoutes } from '@modules/products/infra/express/routes/productRoute';
+import { usersRoutes } from '@modules/users/infra/express/routes/users.routes';
 import { Router } from 'express';
 
 const router = Router();
 
 router.use('/products', productRoutes);
+router.use('/users', usersRoutes);
 
 export { router };
